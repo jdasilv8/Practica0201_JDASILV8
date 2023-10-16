b0aecb581b0cd34662e65fb1fed5218185301880
jorgito <jdasilv8@educacion.navarra.es>
2023-10-16 20:11:02 +0200

    Create Ejercicio3.py

---
+++ Ejercicio3.py
@@ -0,0 +1,5 @@
+nombre = input("Indique su nombre: ")
+nombremayus = nombre.upper()
+nº = len(nombre)
+
+print(nombre , "tiene", nº, "letras, donde", nombremayus, "es el nombre de usuario en mayúsculas y" , nº , "es el número de letras que tienen el nombre.")
