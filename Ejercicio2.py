75a80614e43a9a6417ed123ccd80b61e1865c94b
jorgito <jdasilv8@educacion.navarra.es>
2023-10-16 20:01:06 +0200

    Create Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+nombre = input("Indique su nombre entero: ")
+nombrenormal = nombre.title()
+nombremayus = nombre.upper()
+nombreminus = nombre.lower()
+print (nombrenormal + "\n" + nombremayus + "\n" + nombreminus)
