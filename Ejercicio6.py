3c5f894599d18db97527e4160e4761a365dbb7ff
jorgito <jdasilv8@educacion.navarra.es>
2023-10-19 15:43:49 +0200

    Create Ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,4 @@
+frase = input("Introduzca una frase: ")
+vocal = input("Introduzca una vocal: ")
+frasemayus = frase.replace(vocal,vocal.upper())
+print ("La frase con la vocal:", vocal.upper(), "en mayúscula sería:", frasemayus)
