c22867136d0b342373f838b489925182fc0d8f5f
jorgito <jdasilv8@educacion.navarra.es>
2023-10-20 23:02:07 +0200

    Update Ejercicio11.py

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,20 +1,24 @@
 producto = input("Introduzca el nombre del producto: ")
-precio = input("Introduzca el precio del producto: ")
-unidades = input("Cuántas unidades desea?: ")
+precio = float(input("Introduzca el precio del producto (separar € y céntimos con .: "))
+unidades = int(input("Cuántas unidades desea?: "))
 
-entero , decimal = precio.split(",")
+preciofinal = float(precio * unidades)
+
+precio = str(precio)
+unidades = str(unidades)
+preciofinal = str(preciofinal)
+
+entero , decimal = precio.split(".")
 entero.zfill(6)
 decimal.zfill(2)
 
-preciofinal = float(precio*unidades)
-preciofinal = str(preciofinal)
-enteropf , decimalpf = preciofinal.split(",")
+enteropf , decimalpf = preciofinal.split(".")
 enteropf.zfill(8)
 decimalpf.zfill(2)
 
 
 unidades.zfill(3)
-
+print (entero)
 print ("El producto",producto,
        "Tiene un precio unitario de", entero + "," + decimal,
        " Y", unidades, "Tendrían un coste total de", enteropf + "," + decimalpf)
